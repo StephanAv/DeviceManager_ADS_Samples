e073b3ef13349ec55172166b7606e8c0835e249e
Stephan Avenwedde <StephanA@beckhoff.com>
2022-10-19 13:57:02 +0200

    Python module Windows build prepared

---
+++ PyModule/init.bat
@@ -0,0 +1,3 @@
+set PYTHONPATH=%PYTHONPATH%;..\out\build\x64-Release\PyModule
+python -i main.py
+
--- PyModule/main.py
+++ PyModule/main.py
@@ -1,4 +1,10 @@
 from inspect import getmembers
+import os, sys
 import DeviceManager
 
-print(getmembers(DeviceManager))
+print("sys.path:")
+print('\n'.join(sys.path))
+
+for attr in getmembers(DeviceManager):
+    print(attr)
+
